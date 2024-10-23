3d09db95655a0ac5d354cea1bd0c8fde8aa08e0f
Phuc <duyphuclenguyen@gmail.com>
2024-10-23 18:27:21 -0400

    updated

--- src/Components/CartItem.jsx
+++ src/Components/CartItem.jsx
@@ -9,33 +9,42 @@ const CartItem = ({ onContinueShopping }) => {
 
   // Calculate total amount for all products in the cart
   const calculateTotalAmount = () => {
-    return cart.reduce((total, item) => total + Number(item.cost.substring(1)) * item.quantity, 0)
-  }
+    return cart.reduce((total, item) => total + Number(item.cost.substring(1)) * item.quantity, 0);
+ 
+  };
+
   const handleContinueShopping = (e) => {
-    onContinueShopping(e)
+    onContinueShopping(e);
+   
   };
 
 
 
   const handleIncrement = (item) => {
-    dispatch(updateQuantity({name: item.name, quantity: item.quantity+1}))
+    dispatch(updateQuantity({ name: item.name, quantity: item.quantity + 1 }));
   };
 
   const handleDecrement = (item) => {
-   dispatch(updateQuantity({name: item.name, quantity: item.quantiy-1}))
+    dispatch(updateQuantity({ name: item.name, quantity: item.quantity - 1 }));
+   
   };
 
   const handleRemove = (item) => {
-    dispatch(removeItem(item))
+    dispatch(removeItem(item));
   };
 
   // Calculate total cost based on quantity for an item
   const calculateTotalCost = (item) => {
-    return Number(item.cost.substring(1)) * item.quantity
+    return Number(item.cost.substring(1)) * item.quantity;
   };
+
+  const handleCheckingOut = (e) => {
+    alert('you are checking out')
+  }
 
   return (
     <div className="cart-container">
+      <h2 style={{ color: 'black' }}>Total Plants : {cart.length}</h2>
       <h2 style={{ color: 'black' }}>Total Cart Amount: ${calculateTotalAmount()}</h2>
       <div>
         {cart.map(item => (
@@ -59,7 +68,7 @@ const CartItem = ({ onContinueShopping }) => {
       <div className="continue_shopping_btn">
         <button className="get-started-button" onClick={(e) => handleContinueShopping(e)}>Continue Shopping</button>
         <br />
-        <button className="get-started-button1">Checkout</button>
+        <button className="get-started-button1" onClick={(e) => handleCheckingOut(e)}>Checkout</button>
       </div>
     </div>
   );
--- src/main.jsx
+++ src/main.jsx
@@ -12,3 +12,9 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     </Provider>
   </React.StrictMode>,
 )
+
+/*Import the Provider component from the react-redux library, which allows the Redux store to be accessed by all components in the application.
+
+Import the Redux store from the store.js file.
+
+Wrap the App with the Provider component and pass the Redux store as a prop. This allows every component access to the details of store.js.*/
